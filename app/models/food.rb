183197ed8f157d3ec5adba4290e660e8b314cc3c
ammiller22 <smamiller08@yahoo.com>
2016-07-29 00:35:07 -0700

    adding a few basic tests for the food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,4 @@
 class Food < ActiveRecord::Base
+  validates :name, presence: true
+  validates :glycemic_index, presence: true
 end
--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -4,4 +4,20 @@ class FoodTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @food = Food.new(name: "Pizza", glycemic_index: 53)
+  end
+
+  test "food should be valid" do
+    assert @food.valid?
+  end
+
+  test "food name should be present" do
+    @food.name = "     "
+    assert_not @food.valid?
+  end
+  test "food glycemic_index should be present" do
+    @food.glycemic_index = nil
+    assert_not @food.valid?
+  end
 end
