65e47a27942170ce8c3dd640279239a5c39b9b78
ammiller22 <smamiller08@yahoo.com>
2016-07-29 00:25:55 -0700

    quick tests to validate the model

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,4 @@
 class Exercise < ActiveRecord::Base
+  validates :name, presence: true
+  validates :exercise_index, presence: true
 end
--- test/models/exercise_test.rb
+++ test/models/exercise_test.rb
@@ -4,4 +4,20 @@ class ExerciseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @exercise = Exercise.new(name: "squat", exercise_index: 83)
+  end
+
+  test "exercise should be valid" do
+    assert @exercise.valid?
+  end
+
+  test "exercise name should be present" do
+    @exercise.name = "     "
+    assert_not @exercise.valid?
+  end
+  test "exercise exercise_index should be present" do
+    @exercise.exercise_index = nil
+    assert_not @exercise.valid?
+  end
 end
