a74e7c5034a66ccfed01e5e4c99b83b2e9931be1
ammiller22 <smamiller08@yahoo.com>
2016-07-29 00:31:57 -0700

    WILL REQUIRE MIGRATION - added a food table Fields:    - name    - glycemic_index

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160729071439) do
+ActiveRecord::Schema.define(version: 20160729073059) do
 
   create_table "exercises", force: :cascade do |t|
     t.string   "name"
     t.integer  "exercise_index"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "glycemic_index"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
