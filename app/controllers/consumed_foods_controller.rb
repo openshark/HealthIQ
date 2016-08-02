6710e16bac517fe72d288a9fb931fe3641254d63
ammiller22 <smamiller08@yahoo.com>
2016-08-01 23:56:25 -0700

    Adding the basic functionality to get a new, create, show, and display index for consumed food

--- app/controllers/consumed_foods_controller.rb
+++ app/controllers/consumed_foods_controller.rb
@@ -1,2 +1,35 @@
 class ConsumedFoodsController < ApplicationController
+  def new
+    @consumed_food = ConsumedFood.new
+    @food_list = Food.all
+  end
+
+  def show
+    @consumed_food = ConsumedFood.find(params[:id])
+  end
+
+  def create
+    @consumed_food = ConsumedFood.new(consumed_food_params)
+    if @consumed_food.save
+      flash[:success] = "The food was recorded!"
+      redirect_to @consumed_food
+    else
+      flash[:error] = "Something went wrong!"
+      render 'new'
+    end
+  end
+
+  def index
+    @consumed_foods = ConsumedFood.paginate(page: params[:page])
+  end
+
+
+
+
+
+  private
+
+  def consumed_food_params
+    params.require(:consumed_food).permit(:food_id, :scheduled_date)
+  end
 end
