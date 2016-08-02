0f6bfc26e0bc0c2dddd753f5b557046b5d9a2994
ammiller22 <smamiller08@yahoo.com>
2016-08-01 23:33:38 -0700

    adding in the ability to get a new, create, and show a performed exercise for a specific day

--- app/controllers/performed_exercises_controller.rb
+++ app/controllers/performed_exercises_controller.rb
@@ -2,15 +2,35 @@ class PerformedExercisesController < ApplicationController
 
   def new
     @performed_exercise = PerformedExercise.new
+    @exercise_list = Exercise.all
   end
 
   def show
+    @performed_exercise = PerformedExercise.find(params[:id])
   end
 
   def create
+    @performed_exercise = PerformedExercise.new(performed_exercise_params)
+    if @performed_exercise.save
+      flash[:success] = "The exercise was recorded!"
+      redirect_to @performed_exercise
+    else
+      flash[:error] = "something went wrong!"
+      render 'new'
+    end
   end
 
   def index
     @performed_exercises = PerformedExercise.paginate(page: params[:page])
+  end
+
+
+
+
+
+  private
+
+  def performed_exercise_params
+    params.require(:performed_exercise).permit(:exercise_id, :scheduled_date)
   end
 end
