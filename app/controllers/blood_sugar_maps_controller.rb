ccd3c39cef0922efc90044c962e17b80d9b2ef6f
ammiller22 <smamiller08@yahoo.com>
2016-08-02 17:19:47 -0700

    adding basic functionality for blood sugar maps

--- app/controllers/blood_sugar_maps_controller.rb
+++ app/controllers/blood_sugar_maps_controller.rb
@@ -1,2 +1,36 @@
 class BloodSugarMapsController < ApplicationController
+  def index
+    @blood_sugar_maps = BloodSugarMap.paginate(page: params[:page])
+  end
+
+  def new
+    @blood_sugar_map = BloodSugarMap.new
+  end
+
+  def show
+    @blood_sugar_map = BloodSugarMap.find(params[:id])
+    @exercise_list = Exercise.all
+    @food_list = Food.all
+  end
+
+  def create
+    @blood_sugar_map = BloodSugarMap.new(blood_sugar_map_params)
+    if @blood_sugar_map.save
+      flash[:success] = "The Day was created!"
+      redirect_to @blood_sugar_map
+    else
+      flash[:error] = "Something went wrong!"
+      render 'new'
+    end
+  end
+
+
+
+
+
+  private
+
+    def blood_sugar_map_params
+      params.require(:blood_sugar_map).permit(:tracked_day, :comment)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :foods, only: [:index]
   resources :performed_exercises, only: [:index, :new, :show, :create]
   resources :consumed_foods, only: [:index, :new, :show, :create]
+  resources :blood_sugar_maps, only: [:index, :new, :show, :create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
